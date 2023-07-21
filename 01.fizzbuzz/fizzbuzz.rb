6500982889852e479969549efb8087108bc00e1f
Sadayoshi Tada <tadainfra@gmail.com>
2023-07-22 02:10:47 +0900

    fix loop execution

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,19 +1,12 @@
 #!/usr/bin/env ruby
-require 'debug'
-
-def fizzbuzz(count)
-    if count % 15 == 0 then
-        puts "FizzBuzz"
-    elsif count % 5 == 0 then
-        puts "Buzz"
-    elsif count % 3 == 0 then
-        puts "Fizz"
-    else
-        puts count
-    end
-end
-
 (1..20).each do |count|
-    binding.break
-    fizzbuzz(count)
+	if count % 15 == 0 then
+		puts "FizzBuzz"
+	elsif count % 5 == 0 then
+			puts "Buzz"
+	elsif count % 3 == 0 then
+			puts "Fizz"
+	else
+			puts count
+	end
 end
