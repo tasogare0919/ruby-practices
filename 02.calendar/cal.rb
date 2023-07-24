fbc0731629186b2cdf28379e5f76a2c0038d5091
Sadayoshi Tada <tadainfra@gmail.com>
2023-07-24 20:01:38 +0900

    cosme

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -13,11 +13,11 @@ def main()
   puts "      #{cal_first_day.strftime("%m") + '月'} #{cal_year}"
   puts cal_weekday.join(' ')
   (1..cal_last_day).each do |day|
-          print cal_blank if day == 1
-          print day.to_s.rjust(2) + ' '
-          if (cal_wday_first + day) % 7 == 0
-                  print "\n"
-          end
+    print cal_blank if day == 1
+    print day.to_s.rjust(2) + ' '
+    if (cal_wday_first + day) % 7 == 0
+      print "\n"
+    end
   end
 end 
 
@@ -27,7 +27,6 @@ def cal_params_init
   opt.on('-y year', 'Calendar year designation') { |v| cal_params[:year] = v }
   opt.on('-m month', 'Calendar month designation') { |v| cal_params[:month] = v }
   opt.parse(ARGV)
-
   cal_validate_year(cal_params[:year])
   cal_validate_month(cal_params[:month])
   return cal_params[:year], cal_params[:month] 
