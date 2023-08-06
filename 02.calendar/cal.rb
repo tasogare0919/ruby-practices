6393da15a9f28ef1f27f0c89fceb42d6793d7eb5
Sadayoshi Tada <tadainfra@gmail.com>
2023-08-06 09:40:18 +0900

    trim cal_ prefix

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,18 +4,19 @@ require 'date'
 
 def main
   argv_year, argv_month = year_and_month_from_argv
-  cal_year, cal_month = year_and_month_to_i_with_default(argv_year, argv_month)
-  cal_weekday = ["日", "月", "火", "水", "木", "金", "土"]
-  cal_first_day = Date.new(cal_year, cal_month, 1)
-  cal_last_day = Date.new(cal_year, cal_month, -1).day
-  cal_wday_first = cal_first_day.wday
-  cal_blank = "   " * cal_wday_first
-  puts "      #{cal_first_day.strftime("%m") + '月'} #{cal_year}"
-  puts cal_weekday.join(' ')
-  (1..cal_last_day).each do |day|
-    print cal_blank if day == 1
-    print day.to_s.rjust(2) + ' '
-    if (cal_wday_first + day) % 7 == 0
+  year, month = year_and_month_to_i_with_default(argv_year, argv_month)
+  weekday = ["日", "月", "火", "水", "木", "金", "土"]
+  first_day = Date.new(year, month, 1)
+  last_day = Date.new(year, month, -1).day
+  wday_first = first_day.wday
+  puts(wday_first)
+  blank = "   " * wday_first
+  puts "      #{first_day.strftime("%m") + '月'} #{year}"
+  puts weekday.join(" ")
+  (1..last_day).each do |day|
+    print blank if day == 1
+    print day.to_s.rjust(2) + " "
+    if (wday_first + day) % 7 == 0
       puts
     end
   end
@@ -23,22 +24,22 @@ end
 
 def year_and_month_from_argv
   opt = OptionParser.new
-  cal_params = {}
-  opt.on('-y year', 'Calendar year designation') { |v| cal_params[:year] = v }
-  opt.on('-m month', 'Calendar month designation') { |v| cal_params[:month] = v }
+  params = {}
+  opt.on('-y year', 'Calendar year designation') { |v| params[:year] = v }
+  opt.on('-m month', 'Calendar month designation') { |v| params[:month] = v }
   opt.parse(ARGV)
-  cal_validate_year(cal_params[:year])
-  cal_validate_month(cal_params[:month])
-  return cal_params[:year], cal_params[:month] 
+  validate_year(params[:year])
+  validate_month(params[:month])
+  return params[:year], params[:month] 
 end
 
-def cal_validate_year(year)
+def validate_year(year)
   if year != nil && (year.to_i < 1970 || year.to_i > 2100)
     abort "Invalid year: year #{year} not in range 1970..2100.Input year in range 1970..2100."
   end
 end
 
-def cal_validate_month(month)
+def validate_month(month)
   if month != nil && (month.to_i < 1 || month.to_i > 12)
     abort "Invalid month: #{month} not in range 1..12.Input month in range 1..12."
   end
