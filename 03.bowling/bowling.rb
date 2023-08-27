5213f7d1df1173a0caf0b87c639a490989349b5d
Sadayoshi Tada <tadainfra@gmail.com>
2023-08-27 13:04:28 +0900

    fix: fix suggest condition

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -23,17 +23,17 @@ end
 point = 0
 frames.each.with_index do |frame, i|
   point += if frame[0] == 10 && i < 9
-             if frames[i + 2].nil? == false && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i == 10
-               30
-             elsif frames[i + 1].nil? == false && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i != 10
-               20 + frames[i + 2][0].to_i
-             else
-               10 + frames[i + 1][0].to_i + frames[i + 1][1].to_i
-             end
-           elsif frame.sum == 10 && i < 9
-             10 + frames[i + 1][0].to_i
-           else
-             frame.sum
-           end
+    if !frames[i + 2].nil? && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i == 10
+      30
+    elsif frames[i + 1].nil? == false && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i != 10
+      20 + frames[i + 2][0].to_i
+    else
+      10 + frames[i + 1][0].to_i + frames[i + 1][1].to_i
+    end
+  elsif frame.sum == 10 && i < 9
+    10 + frames[i + 1][0].to_i
+  else
+    frame.sum
+  end
 end
 puts point
