bdb7805929209146125ced14de00a2eed21168af
Sadayoshi Tada <tadainfra@gmail.com>
2023-09-02 16:46:18 +0900

    fix: fix sum scoring section

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'debug'
-
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -22,18 +20,18 @@ end
 
 point = 0
 frames.each.with_index do |frame, i|
-  point += if frame[0] == 10 && i < 9
-    if !frames[i + 2].nil? && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i == 10
-      30
-    elsif frames[i + 1].nil? == false && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i != 10
-      20 + frames[i + 2][0].to_i
-    else
-      10 + frames[i + 1][0].to_i + frames[i + 1][1].to_i
-    end
+  if frame[0] != 10 && frame.sum != 10 || i >= 9
+    point += frame.sum
+  elsif frame[0] == 10 && i < 9
+    point += if !frames[i + 2].nil? && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i == 10
+               30
+             elsif frames[i + 1].nil? == false && frames[i + 1][0].to_i == 10 && frames[i + 2][0].to_i != 10
+               20 + frames[i + 2][0].to_i
+             else
+               10 + frames[i + 1][0].to_i + frames[i + 1][1].to_i
+             end
   elsif frame.sum == 10 && i < 9
-    10 + frames[i + 1][0].to_i
-  else
-    frame.sum
+    point += 10 + frames[i + 1][0].to_i
   end
 end
 puts point
