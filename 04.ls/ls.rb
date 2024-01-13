0b8cd65c598be8915e62dc08136235f757333902
Sadayoshi Tada <tadainfra@gmail.com>
2024-01-14 08:45:23 +0900

    add permission color func,judge file or directory

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,6 +1,28 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-path = Dir.getwd
-print Dir.glob("#{path}/*").map { |f| File.basename(f) }.join(' ')
+require 'fileutils'
+
+path = ARGV[0] || '.'
+color = "\e[0m"
+
+if File.directory?(path)
+  Dir.glob("#{path}/*").each do |file|
+    filename = File.basename(file)
+    color = permission_color(file)
+    print "#{color}#{filename}\e[0m "
+  end
+elsif File.file?(path)
+  color = permission_color(path)
+  print "#{color}#{File.basename(path)}\e[0m "
+end
 print "\n"
+
+def permission_color(file)
+  stat = File::Stat.new(file)
+  if stat.directory?
+    "\e[34m"
+  elsif stat.executable?
+    "\e[31m"
+  end
+end
