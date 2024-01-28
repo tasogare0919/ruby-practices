1fbfe1939f1d18fdc88d0102d89fad5523adea5e
Sadayoshi Tada <tadainfra@gmail.com>
2024-01-28 18:21:45 +0900

    fix get max_length from file name everty column

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -14,19 +14,21 @@ end
 
 if File.directory?(path)
   files = Dir.glob("#{path}/*").sort_by { |file| File.basename(file) }
-  max_length = files.map { |file| File.basename(file).length }.max
   files = files.each_slice((files.size / columns.to_f).ceil).to_a
+  max_length = files.map do |column|
+    column.map { |file| File.basename(file).length }.max
+  end
   files.first.size.times do |i|
     columns.times do |j|
       next unless files[j] && files[j][i]
 
       filename = File.basename(files[j][i])
       color = permission_color(files[j][i])
-      print "#{color}#{filename.ljust(max_length)}\t\e[0m "
+      print "#{color}#{filename.ljust(max_length[j])}\t\e[0m "
     end
     print "\n"
   end
-elsif File.file?(path)
+elsif File.exist?(path)
   color = permission_color(path)
   print "#{color}#{File.basename(path)}\e[0m "
 else
