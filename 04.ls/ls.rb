f6bbe33438c0fdc593eded4b766799d87f88a964
Sadayoshi Tada <tadainfra@gmail.com>
2024-07-20 18:00:27 +0900

    fix rubocop point out

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -15,27 +15,34 @@ COMMAND_OPTIONS = options
 COLUMNS = 3
 
 def file_mode(mode)
-  type = case mode & 0170000
-         when 0140000 then 's'
-         when 0120000 then 'l'
-         when 0100000 then '-'
-         when 0060000 then 'b'
-         when 0040000 then 'd'
-         when 0020000 then 'c'
-         else '?'
-         end
+  type = file_type(mode)
+  permission = file_permission(mode)
+  "#{type}#{permission}"
+end
 
-  permission = ['r', 'w', 'x'].map do |p|
+def file_type(mode)
+  case mode & 0o170000
+  when 0o140000 then 's'
+  when 0o120000 then 'l'
+  when 0o100000 then '-'
+  when 0o060000 then 'b'
+  when 0o040000 then 'd'
+  when 0o020000 then 'c'
+  else '?'
+  end
+end
+
+def file_permission(mode)
+  %w[r w x].map do |p|
     [4, 2, 1].map { |b| mode & b != 0 ? p : '-' }.join
   end.join
-
-  "#{type}#{permission}"
 end
 
 def calculate_total_blocks(path)
   total = 0
   Dir.glob(File.join(path, '*'), File::FNM_DOTMATCH) do |file|
     next if File.basename(file) == '.' || File.basename(file) == '..' || File.basename(file).start_with?('.')
+
     stat = File.lstat(file)
     total += stat.blocks
   end
@@ -55,14 +62,45 @@ end
 def print_files(files, display_max_lengths)
   files.first.size.times do |first_row_count|
     if COMMAND_OPTIONS[:l]
-      file_path = files[0][first_row_count]
-      stat = File.stat(file_path)
-      puts "#{file_mode(stat.mode)} #{stat.nlink} #{Etc.getpwuid(stat.uid).name} #{Etc.getgrgid(stat.gid).name} #{stat.size.to_s.rjust(8)} #{stat.mtime.strftime('%b %d %H:%M')} #{File.basename(file_path)}"
+      print_file_long_format(files, first_row_count)
     else
       print_file_by_column(files, first_row_count, display_max_lengths)
       print "\n"
     end
   end
+end
+
+def print_file_long_format(files, first_row_count)
+  file_info = fetch_file_info(files[0][first_row_count])
+  puts file_info.join(' ')
+end
+
+def fetch_file_info(file_path)
+  stat = File.stat(file_path)
+  mode = file_mode(stat.mode)
+  nlink = stat.nlink
+  uid = fetch_uid(stat.uid)
+  gid = fetch_gid(stat.gid)
+  size = fetch_size(stat.size)
+  mtime = fetch_mtime(stat.mtime)
+  basename = File.basename(file_path)
+  [mode, nlink, uid, gid, size, mtime, basename]
+end
+
+def fetch_uid(uid)
+  Etc.getpwuid(uid).name
+end
+
+def fetch_gid(gid)
+  Etc.getgrgid(gid).name
+end
+
+def fetch_size(size)
+  size.to_s.rjust(8)
+end
+
+def fetch_mtime(mtime)
+  mtime.strftime('%b %d %H:%M')
 end
 
 def sort_files(files)
@@ -71,7 +109,7 @@ end
 
 def fetch_and_sort_files(path)
   files = Dir.glob("#{path}/*")
-  sorted_files = sort_files(files)
+  sort_files(files)
 end
 
 def display_max_lengths(files)
@@ -92,16 +130,24 @@ end
 def handle_directory(path)
   files = fetch_and_sort_files(path)
   if COMMAND_OPTIONS[:l]
-    total_blocks = calculate_total_blocks(path)
-    puts "total #{total_blocks}"
-    files.each do |file|
-      print_files([[file]], [] )
-    end
+    handle_long_format(path, files)
   else
-    files = files.each_slice((files.size / COLUMNS.to_f).ceil).to_a
-    display_max_lengths = display_max_lengths(files)
-    print_files(files, display_max_lengths)
+    handle_column_format(files)
   end
+end
+
+def handle_long_format(path, files)
+  total_blocks = calculate_total_blocks(path)
+  puts "total #{total_blocks}"
+  files.each do |file|
+    print_files([file], [])
+  end
+end
+
+def handle_column_format(files)
+  files = file.each.slice((files.size / COLUMNS.to_f).ceil).to_a
+  display_max_lengths = display_max_lengths(files)
+  print_files(files, display_max_lengths)
 end
 
 def handle_file(path)
